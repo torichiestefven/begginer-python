dadeb71480ce91ab7593370accb8c1209fc3aa20
Stefven Torichie <64120065+torichiestefven@users.noreply.github.com>
2020-11-17 18:07:56 +0700

    Update tebak password.py

--- tebak password.py
+++ tebak password.py
@@ -2,34 +2,32 @@ import random
 
 jawaban = random.randint(1000000000,9999999999)
 tebakan = 0
-kesempatan = 50
+kesempatan = 20
 
-print("DOKUMEN NEGARA")
-print("SANGAT RAHASIA")
 print("SECURITY SYSTEM")
 
 while tebakan!=jawaban and kesempatan>0 :
-    tebakan=int(input("Masukkan Password[10 digit]: "))
+    tebakan=int(input("Please enter the Password[10 digit]: "))
     if tebakan<jawaban and tebakan<=9999999999 and tebakan>=1000000000:
-        print("Silahkan input password yang lebih tinggi lagi")
+        print("Please input higher password ammount")
         kesempatan-=1
         print("Sisa kesempatan:",kesempatan)
         print()
     elif tebakan>jawaban and tebakan<=9999999999 and tebakan>=1000000000:
-        print("Silahkan input password yang lebih rendah lagi")
+        print("Please input lower password ammount")
         kesempatan-=1
         print("Sisa kesempatan:",kesempatan)
         print()
     elif tebakan==jawaban:
-        print("Password anda benar")
+        print("Your password is correct ! ")
         kesempatan-=1
         print("Sisa kesempatan:",kesempatan)
         print()
         while 1!=0:
-            print("Congratz")
+            print("Congratz, you win the game")
     else:
-        print("Input yang benar woi")
+        print("Harap masukkan format yang benar")
 if kesempatan==0:
-    print("Anda kehabisan kesempatan. Password yang benar adalah",jawaban)
-    print("Dasar Hacker NOOB")
+    print("You have no chance, the correct password is ",jawaban)
+    
 
